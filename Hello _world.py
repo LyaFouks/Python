4ef88ec6930eefc19677e3b53be74c03141c4970
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-17 19:16:43 +0300

    Strings in Python

--- Hello _world.py
+++ Hello _world.py
@@ -1,16 +1,33 @@
-x = 5
-y = 3
+greeting = "Hello"
+first_name = "Jack"
+last_name = 'White'
+exclamation_sign = "!"
+white_space = " "
+whole_sentence = greeting + white_space + first_name + white_space \
+                 + last_name + exclamation_sign
+print(whole_sentence)
+long_string = "This is the long string"
+print(long_string)
 
-# print(x + y)
-# print(x - y)
-# print(x * y)
-# print(x / y)
-# print(x // y)
-# print(x ** y)
-# print(x % y)
+#Escaping \
+some_string = 'I\'m a programmer'
+print(some_string)
+another_string = "I want to learn \"Python\""
+print(another_string)
 
-credit = 1000
-credit_rate = 10
-number_of_years = 5
-final_sum = credit + credit / 100 * credit_rate * number_of_years
-print(final_sum)
+string_with_new_lines = "Hello! \n   \rMy name is Nikita"
+print(string_with_new_lines)
+
+numbers = "1\t2345\t67"
+print(numbers)
+some_text = "\tHello! \n\tI'm very glad to see you!"
+print(some_text)
+
+#Triple quotes
+string_with_triple_quotes = """This is '
+text with 
+"triple 
+quotes" """
+another_string_with_triple_quotes = '''This is ' text with "triple quotes" '''
+print(string_with_triple_quotes)
+print(another_string_with_triple_quotes)
