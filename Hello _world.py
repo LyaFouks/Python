afc3bcf9da79a8e7502694ef21d3db6b9fc65a49
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-17 17:16:02 +0300

    Variables

---
+++ Hello _world.py
@@ -0,0 +1,16 @@
+x = 5
+y = 3
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x ** y)
+# print(x % y)
+
+credit = 1000
+credit_rate = 10
+number_of_years = 5
+final_sum = credit + credit / 100 * credit_rate * number_of_years
+print(final_sum)
