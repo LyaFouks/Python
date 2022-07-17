2d8018fb2e6f202862ee5350847bb4a745c8625b
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-17 19:53:10 +0300

    Indexing and Slicing Strings

---
+++ IndexingAndSlicinginStrings/indexing_and_slicing.py
@@ -0,0 +1,20 @@
+greeting = "Hello Python!"
+greeting_length = len(greeting)
+print(len(greeting))
+
+# Indexing
+print(greeting[6])
+print(greeting[-1])
+print(greeting[12])
+print(greeting[-4])
+
+# Slicing
+print(greeting[2:5])
+print(greeting[6:10])
+print(greeting[-5:-2])
+print(greeting[4:])
+print(greeting[:5])
+print(greeting[:])
+print(greeting[2::3])
+print(greeting[1:8:2])
+print(greeting[::-1])
