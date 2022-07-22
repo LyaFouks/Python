43bb359617d0c79295533f5628f26ddc8aeb6012
LyaFouks <106815417+LyaFouks@users.noreply.github.com>
2022-07-22 07:41:27 +0300

    Condotional operators IF ELIF ELSE

---
+++ ConditionalOperators/conditional_operators.py
@@ -0,0 +1,56 @@
+# x = 4
+#
+# if x > 3:
+#     print('x > 3')
+#     print('I\'m happy')
+# elif x < 3:
+#     print('x < 3')
+# else:
+#     print('x == 3')
+
+# day_time = 'midnight'
+#
+# if day_time == 'morning':
+#     print('Monster wakes up.')
+# elif day_time == 'afternoon':
+#     print('Monster is walking.')
+# elif day_time == 'evening':
+#     print('Monster is eating.')
+# elif day_time == 'night':
+#     print('Monster is sleeping.')
+# else:
+#     print('Monster is doing something.')
+
+# x = 16
+# if x % 2 == 0:
+#     print('x is even')
+# else:
+#     print('x is odd')
+# print('Some text')
+
+# user_input = input('Input something')
+# if user_input == 'Hello':
+#     print('Hello! Nice to meet you!')
+
+# False: 0, empty string, None, empty object
+
+# if 1:
+#     print('Something')
+# elif 0:
+#     print('Something1')
+# elif '':
+#     print('Something2')
+# elif 'fadfadf':
+#     print('Something3')
+# elif None:
+#     print('Something4')
+# elif []:
+#     print('Something5')
+# elif [1, 2]:
+#     print('Something5')
+
+lucky_number = input('Enter a number')
+if lucky_number:
+    print(lucky_number + ' is your lucky number!')
+else:
+    print('You have to enter some number.')
